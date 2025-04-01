56482ae7f3a2fae30f6e06672b0a60e0f833bad6
music-lover-579 <fireworks_579@sjtu.edu.cn>
2025-04-01 21:21:12 +0800

    Included PS2X library

---
+++ Controller.h
@@ -0,0 +1,13 @@
+/*
+ * This header contains functions that convert the PS2X controller input into actions 
+ */
+
+#ifndef _CONTROLLER_H_
+#define _CONTROLLER_H_
+
+#include "PS2X_lib.h"
+#include "Globals.h"
+
+extern PS2X ps2x; // create PS2 Controller Class
+
+#endif // _CONTROLLER_H_
